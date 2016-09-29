0a0778b636310121686393846981e1df412aa4b1
GalateaRaj <basu@galatea-associates.com>
2016-09-29 17:07:28 -0400

    Added log4j

--- src/main/java/org/galatea/starter/web/SettlementController.java
+++ src/main/java/org/galatea/starter/web/SettlementController.java
@@ -5,6 +5,8 @@ import lombok.ToString;
 
 import org.galatea.starter.domain.SettlementMission;
 import org.galatea.starter.domain.rpsy.ISettlementMissionRpsy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +18,14 @@ import java.util.List;
 @EqualsAndHashCode
 public class SettlementController {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(SettlementController.class);
+
   @Autowired
   ISettlementMissionRpsy missionrpsy;
 
   @RequestMapping("/save")
   public SettlementMission saveMission() {
+    LOGGER.info("Saving mission");
     SettlementMission m =
         SettlementMission.builder().externalParty("GS").depot("DTC").qty(100).build();
     missionrpsy.save(m);
