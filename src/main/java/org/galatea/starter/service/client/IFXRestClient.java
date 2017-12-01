2a6d7e63fa53663eafe8177fc430902173539eb3
Chris Bond <c.bond91@yahoo.com>
2017-12-01 16:59:53 +0000

    Switch to using @PathVariable in IFXRestClient to fix URL bug

--- src/main/java/org/galatea/starter/service/client/IFXRestClient.java
+++ src/main/java/org/galatea/starter/service/client/IFXRestClient.java
@@ -3,6 +3,7 @@ package org.galatea.starter.service.client;
 import org.galatea.starter.FXRestClientConfig;
 import org.galatea.starter.domain.FXRateResponse;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,6 @@ public interface IFXRestClient {
     // which returns {"base":"GBP","date":"2017-11-30","rates":{"USD":1.3467}}
     @RequestMapping(method = RequestMethod.GET,
         value="/latest?base={base}&symbols=USD")
-    FXRateResponse rate(@RequestParam("base") String base);
+    FXRateResponse rate(@PathVariable("base") String base);
 
 }
