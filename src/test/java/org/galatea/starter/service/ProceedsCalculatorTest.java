a62fdc8ab83805da49091fb40d9e033078f8b8cf
Jonathan Evans <evans@galatea-associates.com>
2017-12-20 10:21:00 +0000

    Added tests for ProceedsCalculator

---
+++ src/test/java/org/galatea/starter/service/ProceedsCalculatorTest.java
@@ -0,0 +1,62 @@
+package org.galatea.starter.service;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.galatea.starter.ASpringTest;
+import org.galatea.starter.domain.FXRateResponse;
+import org.galatea.starter.service.client.IFXRestClient;
+import org.joda.money.BigMoney;
+import org.joda.money.CurrencyUnit;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import static org.mockito.BDDMockito.given;
+
+public class ProceedsCalculatorTest extends ASpringTest {
+
+	@MockBean
+	private IFXRestClient restClient;
+
+	private IProceedsCalculator proceedsCalc;
+
+	private BigMoney proceeds;
+
+	private BigMoney USDProceeds;
+
+	@Before
+	public void setUp() {
+		proceedsCalc = new ProceedsCalculator(restClient);
+		proceeds = BigMoney.of(CurrencyUnit.of("GBP"), BigDecimal.valueOf(100));
+	}
+
+	@Test
+	public void testGetUSDProceedsSuccess() {
+
+		given(restClient.getRate(proceeds.getCurrencyUnit().getCode()))
+				.willReturn(FXRateResponse.builder().baseCurrency(CurrencyUnit.of("GBP"))
+						.exchangeRate(BigDecimal.valueOf(1.33d)).validOn(new Date()).build());
+
+		USDProceeds = proceedsCalc.getUSDProceeds(proceeds);
+
+		// this works but doesn't feel correct 
+		assertTrue(BigMoney.of(CurrencyUnit.of("USD"), 133d).compareTo(USDProceeds) == 0);
+
+		// this does not work -> expected <USD 133> but was <USD 133.00>
+		// assertEquals(BigMoney.of(CurrencyUnit.of("USD"), 133d), USDProceeds);
+
+	}
+
+	@Test
+	public void testGetUSDProceedsFromUSD() {
+		// case when the initial currency is USD already, so we expect the same result
+		// back.
+		proceeds = BigMoney.of(CurrencyUnit.of("USD"), BigDecimal.valueOf(100));
+		USDProceeds = proceedsCalc.getUSDProceeds(proceeds);
+
+		assertEquals(proceeds, USDProceeds);
+	}
+
+}
