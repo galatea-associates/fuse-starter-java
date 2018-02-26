01aca3cc743a77b67a0fce6767ddbd9915a43917
James Barber <jbarber@2017-COMPUTER8.Galatea.com>
2018-02-26 11:40:14 -0500

    Issue #138 Class w/ sonar issues, for evaluating sonar's short-lived branch strategy.

---
+++ src/main/java/org/galatea/starter/service/SonarIssuesAboundHere.java
@@ -0,0 +1,39 @@
+
+package org.galatea.starter.service;
+
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+
+@RequiredArgsConstructor
+@Slf4j
+@ToString
+@EqualsAndHashCode
+public class SonarIssuesAboundHere {
+
+  public static int someInt = 23;
+
+  public void methodWithNoTests(double d) {
+    for (int i = 0; i < d; i++) {
+      log.info("i");
+      if (i < 10) {
+        log.info("i is less than 10");
+      } else if (i > 3) {
+        log.info("really i > 9...");
+      }
+    }
+  }
+
+
+  // seems like sonar should complain about a main method in some rando class...
+  public static void main(String[] args) {
+    // bad name, magic number, and let's not use this either!
+    int i = 34;
+
+    String string = new String("args!");
+    System.out.println("My string: " + string);
+
+  }
+}
