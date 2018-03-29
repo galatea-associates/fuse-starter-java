29bb0ed4b4539968cedb84db4c269084022bcbfd
Sam Lightfoot <lightfoot@galatea-associates.com>
2018-03-29 15:36:09 +0100

    Fix MessageUtil import

--- src/test/java/org/galatea/starter/MessageUtil.java
+++ src/test/java/org/galatea/starter/MessageUtil.java
@@ -1,18 +1,17 @@
 package org.galatea.starter;
 
 import com.googlecode.protobuf.format.JsonFormat;
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 
 import org.galatea.starter.entrypoint.messagecontracts.Messages;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 public class MessageUtil {
 
   public static Messages.TradeAgreementMessage jsonToTradeAgreement(String json)
       throws IOException {
-    byte[] bytes = json.getBytes();
-    ByteInputStream bis = new ByteInputStream(json.getBytes(), bytes.length);
+    ByteArrayInputStream bis = new ByteArrayInputStream(json.getBytes());
     Messages.TradeAgreementMessage.Builder builder = Messages.TradeAgreementMessage.newBuilder();
     new JsonFormat().merge(bis, builder);
     return builder.build();
