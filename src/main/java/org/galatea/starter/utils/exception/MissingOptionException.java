bbf77a46f508d16fa721e09a58118206dfb085bd
Kathryn Tweel <tweel@galatea-associates.com>
2018-09-21 14:31:17 -0400

    show how to define port for spring boot application

---
+++ src/main/java/org/galatea/starter/utils/exception/MissingOptionException.java
@@ -0,0 +1,8 @@
+package org.galatea.starter.utils.exception;
+
+public class MissingOptionException extends RuntimeException{
+
+  public MissingOptionException(String message) {
+    super(message);
+  }
+}
