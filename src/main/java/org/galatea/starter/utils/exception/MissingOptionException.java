7adfb866d220b6394d6d52a5cb4d0de8efa0d5e7
Kathryn Tweel <tweel@galatea-associates.com>
2018-09-21 14:43:43 -0400

    javadoc to MissingOptionException

--- src/main/java/org/galatea/starter/utils/exception/MissingOptionException.java
+++ src/main/java/org/galatea/starter/utils/exception/MissingOptionException.java
@@ -1,7 +1,14 @@
 package org.galatea.starter.utils.exception;
 
+/**
+ * Thrown when option argument expected via command line is not found.
+ */
 public class MissingOptionException extends RuntimeException{
 
+  /**
+   * Exception indicating expected command line argument not found.
+   * @param message exception message
+   */
   public MissingOptionException(String message) {
     super(message);
   }
