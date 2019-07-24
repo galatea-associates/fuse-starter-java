f0ab432c5c1f83121c4b4b41bf862e0e98b6169c
neongreen183 <Rborr@rollins.edu>
2019-07-24 16:48:12 -0400

    TickerData now returns will all days, in reverse order

--- src/main/java/org/galatea/starter/TickerInfo/Ticker.java
+++ src/main/java/org/galatea/starter/TickerInfo/Ticker.java
@@ -3,24 +3,28 @@ package org.galatea.starter.TickerInfo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.SortedMap;
+
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Ticker {
     @JsonProperty("Meta Data")
     public MetaData metaData;
     @JsonProperty("Time Series (Daily)")
-    public TimeSeries timeSeries;
+    public SortedMap<String, Day> timeSeries;
 
-    public Ticker(){
-
-    }
-
-    public TimeSeries getTimeSeries() {
+    public SortedMap<String, Day> getTimeSeries() {
         return timeSeries;
     }
 
-    public void setTimeSeries(TimeSeries timeSeries) {
+    public void setTimeSeries(SortedMap<String, Day> timeSeries) {
         this.timeSeries = timeSeries;
+    }
+
+    public Ticker(){
+
     }
 
     public MetaData getMetaData() {
