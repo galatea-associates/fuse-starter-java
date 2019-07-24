a1a9ab336c48369bf73dcdb74f11eca8083b5e07
neongreen183 <Rborr@rollins.edu>
2019-07-24 09:52:01 -0400

    Added getdate method to alpha vantage service

--- src/main/java/org/galatea/starter/TickerInfo/AlphaVantageService.java
+++ src/main/java/org/galatea/starter/TickerInfo/AlphaVantageService.java
@@ -4,6 +4,10 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 
 
@@ -20,4 +24,15 @@ public class AlphaVantageService {
        Ticker tickerdata = restTemplate.getForObject(AlphaVantageService.getURL(ticker,days), Ticker.class);
        return tickerdata;
    }
+
+   public static String getDate(int i){
+
+       DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+       Calendar cal = Calendar.getInstance();
+       cal.add(Calendar.DAY_OF_YEAR,i*-1);
+       return dateFormat.format(cal.getTime());
+
+
+   }
+
 }
