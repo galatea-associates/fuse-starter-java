6bbb749e3fb67a18ae68c3866fc14a650947072a
neongreen183 <Rborr@rollins.edu>
2019-07-24 16:36:15 -0400

    Added example mongo db access to TickerInfoRepository. Gives error. Added MongoDB to POM

--- src/main/java/org/galatea/starter/TickerInfo/TickerInfoRepository.java
+++ src/main/java/org/galatea/starter/TickerInfo/TickerInfoRepository.java
@@ -1,8 +1,35 @@
 package org.galatea.starter.TickerInfo;
 
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import java.lang.Object;
+import org.bson.Document;
+
+
 public class TickerInfoRepository {
 
     public static void main(String [] args){
         System.out.println("https://spring.io/guides/gs/accessing-data-mongodb/");
+
+        MongoClientURI uri = new MongoClientURI(
+                "mongodb+srv://ReneBorr:<GalaPassword>@tickerinfo-glh7c.mongodb.net/test?retryWrites=true&w=majority");
+        MongoClient mongoClient = new MongoClient(uri);
+        MongoDatabase database = mongoClient.getDatabase("AlphaVantage");
+
+
+
+        MongoCollection col = database.getCollection("Tickers");
+        Document doc = new Document();
+        doc.append("Name", "Rene");
+        col.insertOne(doc);
+
+
+
+
+
+
     }
 }
