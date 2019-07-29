1fe5fc1690ade29cd9e37950b33c4003f3aa51cf
Rborr <Rborr@rollins.edu>
2019-07-29 15:11:52 -0400

    Updated MongoDB driver version and added example code for connection to MongoDB atlas

--- src/main/java/org/galatea/starter/TickerInfo/TickerInfoRepository.java
+++ src/main/java/org/galatea/starter/TickerInfo/TickerInfoRepository.java
@@ -3,11 +3,14 @@ package org.galatea.starter.TickerInfo;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import java.lang.Object;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.HashMap;
@@ -16,8 +19,29 @@ import java.util.TreeMap;
 import java.util.SortedMap;
 import org.bson.Document;
 
+import org.springframework.web.client.RestTemplate;
+
 
 public class TickerInfoRepository {
+
+
+    public static void main(String [] args){
+
+        //Obtain MongoDB Collection
+        MongoClientURI uri = new MongoClientURI("mongodb+srv://ReneBorr:GalaPassword@tickerinfo-glh7c.mongodb.net/admin?authSource=admin");
+        MongoClient mongoClient = new MongoClient(uri);
+        MongoDatabase database = mongoClient.getDatabase("AlphaVantage");
+        MongoCollection col = database.getCollection("Tickers");
+        //rest Template
+        RestTemplate restTemplate = new RestTemplate();
+        //InfoRepository
+        TickerInfoRepository repo = new TickerInfoRepository();
+
+
+        Ticker ticker = repo.getTicker("AAPL",20);
+
+        //col.insertOne(ticker);
+    }
 
 
     public Ticker getTicker(String ticker, int days){
