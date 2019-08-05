b3535773d63668fef64c906a5dac9f6abc516806
Eric Collins <ecollins@Galatea.com>
2019-08-05 14:19:42 -0400

    #23 -- use WireMock in IEX service unit tests

--- src/main/java/org/galatea/starter/domain/IexLastTradedPrice.java
+++ src/main/java/org/galatea/starter/domain/IexLastTradedPrice.java
@@ -3,11 +3,13 @@ package org.galatea.starter.domain;
 import lombok.Builder;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 @Builder
 public class IexLastTradedPrice {
   private String symbol;
-  private long price;
+  private BigDecimal price;
   private Integer size;
   private long time;
 }
--- src/test/java/org/galatea/starter/entrypoint/IexRestControllerTest.java
+++ src/test/java/org/galatea/starter/entrypoint/IexRestControllerTest.java
@@ -1,92 +1,78 @@
 package org.galatea.starter.entrypoint;
 
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import junitparams.JUnitParamsRunner;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.galatea.starter.ASpringTest;
-import org.galatea.starter.domain.IexLastTradedPrice;
-import org.galatea.starter.domain.IexSymbol;
-import org.galatea.starter.service.IexService;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.WireMockSpring;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.math.BigDecimal;
+
 
 @RequiredArgsConstructor
 @Slf4j
-// We don't load the entire spring application context for this test.
-@WebMvcTest(IexRestController.class)
+// We need to do a full application start up for this one, since we want the feign clients to be instantiated.
+// It's possible we could do a narrower slice of beans, but it wouldn't save that much test run time.
+@SpringBootTest
+// this gives us the MockMvc variable
+@AutoConfigureMockMvc
 // Use this runner since we want to parameterize certain tests.
 // See runner's javadoc for more usage.
 @RunWith(JUnitParamsRunner.class)
 public class IexRestControllerTest extends ASpringTest {
 
-  @Autowired
-  private MockMvc mvc;
+    @ClassRule
+    // there are other ways to enable WireMock, including the spring boot @EnableWireMock annotation, but the ClassRule is pretty simple,
+    // and follows the pattern we use in ASpringTest.
+    public static WireMockClassRule wiremock = new WireMockClassRule(WireMockSpring.options()
+            // this port needs to match the spring.rest.iexBasePath url in application.yml for the test profile.
+            // you can configure WireMock to use a random port, and bind that port to a system variable as an alternative.
+            .port(9938)
+            // point at the json mappings and stubs held in files.  There are a variety of schemes for representing the WireMock stubs.
+            .usingFilesUnderClasspath("wiremock"));
 
-  @MockBean
-  private IexService mockIexService;
+    @Autowired
+    private MockMvc mvc;
 
-  @Test
-  public void testGetSymbolsEndpoint() throws Exception {
+    @Test
+    public void testGetSymbolsEndpoint() throws Exception {
 
-    List<IexSymbol> mockResponse = new ArrayList<>();
-    mockResponse.add(IexSymbol.builder()
-        .symbol("SNAP")
-        .name("Snapchat")
-        .date(new Date())
-        .isEnabled(true)
-        .type("test")
-        .iexId("123")
-        .build());
+        MvcResult result = this.mvc.perform(
+                // note that we were are testing the fuse REST end point here, not the IEX end point.
+                // the fuse end point in turn calls the IEX end point, which is WireMocked for this test.
+                org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get("/iex/symbols")
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                // some simple validations, in practice I would expect these to be much more comprehensive.
+                .andExpect(jsonPath("$[0].symbol", is("A")))
+                .andExpect(jsonPath("$[1].symbol", is("AA")))
+                .andExpect(jsonPath("$[2].symbol", is("AAAU")))
+                .andReturn();
+    }
 
-    given(this.mockIexService.getAllSymbols()).willReturn(mockResponse);
+    @Test
+    public void testGetLastTradedPrice() throws Exception {
 
-    MvcResult result = this.mvc.perform(
-        get("/iex/symbols").accept(MediaType.APPLICATION_JSON_VALUE))
-        .andExpect(status().isOk())
-        .andReturn();
-
-    final String jsonResponse = result.getResponse().getContentAsString();
-  }
-
-  @Test
-  public void testGetLastTradedPrice() throws Exception {
-
-    List<IexLastTradedPrice> mockResponse = new ArrayList<>();
-
-    mockResponse.add(IexLastTradedPrice.builder()
-        .symbol("SNAP")
-        .price(123L)
-        .size(1)
-        .time(12908371L)
-        .build());
-
-    given(this.mockIexService.getLastTradedPriceForSymbols(Collections.singletonList("SNAP")))
-        .willReturn(mockResponse);
-
-    MvcResult result = this.mvc.perform(
-        get("/iex/lastTradedPrice")
-            .param("symbols", "SNAP")
-            .accept(MediaType.APPLICATION_JSON_VALUE))
-        .andExpect(status().isOk())
-        .andReturn();
-
-    final String jsonResponse = result.getResponse().getContentAsString();
-
-  }
+        MvcResult result = this.mvc.perform(
+                org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get("/iex/lastTradedPrice?symbols=FB")
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].symbol", is("FB")))
+                .andExpect(jsonPath("$[0].price").value(new BigDecimal("182.305")))
+                .andReturn();
+    }
 }
