3776132eec66e76649ab5dcf9f7e2afcbe159d5a
Thomas Edwards <edwards@galatea-associates.com>
2019-08-14 18:06:01 +0100

    Break up long comments to fit 100 character line limit

--- src/main/java/org/galatea/starter/entrypoint/BaseRestController.java
+++ src/main/java/org/galatea/starter/entrypoint/BaseRestController.java
@@ -18,8 +18,10 @@ public abstract class BaseRestController {
    * Adds the specified requestId to the context for this request (if not null).
    */
   protected void processRequestId(final String requestId) {
-    //This is a temporary solution for cleaning special characters from the REST request params (and will not fix newlines in the parameters or JSON body being printed by the @Log annotation)
-    //Proper fix requires version 2.10.0 or higher of the log4j dependencies.  See Issue #243 for more information.
+    //This is a temporary solution for cleaning special characters from the REST request params
+    //It will not fix newlines in the parameters or JSON body being printed by the @Log annotation
+    //Proper fix requires version 2.10.0 or higher of the log4j dependencies
+    //See Issue #243 for more information.
     if (requestId != null) {
       String cleanedRequestId = null;
       try {
