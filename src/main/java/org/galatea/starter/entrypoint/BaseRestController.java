c513f0dbf0fbaf3771074cea567933ac76f84cb6
Thomas Edwards <edwards@galatea-associates.com>
2019-08-21 12:54:23 +0100

    Code Review Changes

--- src/main/java/org/galatea/starter/entrypoint/BaseRestController.java
+++ src/main/java/org/galatea/starter/entrypoint/BaseRestController.java
@@ -1,6 +1,6 @@
 package org.galatea.starter.entrypoint;
 
-import java.io.UnsupportedEncodingException;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.aspect4log.Log;
 import net.sf.aspect4log.Log.Level;
@@ -17,21 +17,15 @@ public abstract class BaseRestController {
   /**
    * Adds the specified requestId to the context for this request (if not null).
    */
+  @SneakyThrows
   protected void processRequestId(final String requestId) {
     //This is a temporary solution for cleaning special characters from the REST request params
     //It will not fix newlines in the parameters or JSON body being printed by the @Log annotation
     //Proper fix requires version 2.10.0 or higher of the log4j dependencies
     //See Issue #243 for more information.
     if (requestId != null) {
-      String cleanedRequestId;
-      try {
-        cleanedRequestId = UriUtils.encode(requestId, "ISO-8859-1");
-        log.info("Request received.  Cleaned id: {}", cleanedRequestId);
-      } catch (UnsupportedEncodingException e) {
-        log.error("Unable to clean requestId, uncleaned version will be used in logs", e);
-        log.info("Request received with id: {}", requestId);
-        cleanedRequestId = requestId;
-      }
+      String cleanedRequestId = UriUtils.encode(requestId, "UTF-8");
+      log.info("Request received.  Cleaned id: {}", cleanedRequestId);
       Tracer.setExternalRequestId(cleanedRequestId);
     }
   }
