5bbe0b5064265dfe745b3f3fcce0673f2a7aa6e9
GalaHarley <harley@galatea-associates.com>
2019-08-21 13:50:23 -0400

    #288: put cleaned request in MDC

--- src/main/java/org/galatea/starter/entrypoint/BaseRestController.java
+++ src/main/java/org/galatea/starter/entrypoint/BaseRestController.java
@@ -30,7 +30,7 @@ public abstract class BaseRestController {
       log.info("Request received.  Cleaned id: {}", cleanedRequestId);
       // And add to MDC so it will show up in the logs
       // The key used here must align with the key defined in the logging config's log-pattern
-      MDC.put(EXTERNAL_REQUEST_ID, requestId + " - ");
+      MDC.put(EXTERNAL_REQUEST_ID, cleanedRequestId + " - ");
     }
   }
 }
