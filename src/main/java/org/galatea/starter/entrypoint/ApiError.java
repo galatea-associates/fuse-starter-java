a4348fec0fa52d1da784e40827c5ec1de7399769
Evalyn Berleant <eberleant@gmail.com>
2020-06-17 15:19:23 -0400

    Enable ApiError conversion to XML

--- src/main/java/org/galatea/starter/entrypoint/ApiError.java
+++ src/main/java/org/galatea/starter/entrypoint/ApiError.java
@@ -1,7 +1,12 @@
 package org.galatea.starter.entrypoint;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.ToString;
 import org.springframework.http.HttpStatus;
@@ -9,6 +14,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE) // for jackson
+@XmlRootElement(name = "error")
+@XmlAccessorType(XmlAccessType.FIELD) // required if using lombok to avoid duplicate properties
 public class ApiError {
 
   @NonNull
