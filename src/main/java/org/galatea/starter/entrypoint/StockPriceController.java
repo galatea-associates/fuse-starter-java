7cbf6f74875e2f1842ea0acff7c4ca1bbfbc7123
Samuel O. Ugowe <sdougowe@gmail.com>
2020-09-10 15:03:45 -0400

    Finished Jackson integration
    -modified MongoDocument and removed 'volume' property/field
    +completed 'days'-limited results output

--- src/main/java/org/galatea/starter/entrypoint/StockPriceController.java
+++ src/main/java/org/galatea/starter/entrypoint/StockPriceController.java
@@ -1,5 +1,9 @@
 package org.galatea.starter.entrypoint;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import java.util.HashMap;
@@ -40,6 +44,28 @@ public class StockPriceController extends BaseRestController {
     }
 
     TreeMap<String, MongoDocument> processed = alphaVantageService.access(ticker, days);
-    return processed.descendingMap().toString();
+    // constructing the array of 'days'-limited stock price results
+    ObjectMapper objectMapper = new ObjectMapper();
+    ObjectNode root = objectMapper.createObjectNode();
+    int iterations = days > processed.size() ? processed.size() : days;
+    ArrayNode jsonArrayRoot = root.putArray(String.format("%s Daily Stock Prices (%d)",
+        ticker.toUpperCase(), iterations));
+    String key = processed.lastKey();
+    // serializes each entry as a JSON object, up to iterations
+    for (int i = 0; i < iterations; i++) {
+      ObjectNode objectNode = objectMapper.createObjectNode();
+      objectNode.put("date", key);
+      objectNode.putPOJO("prices", processed.get(key)); // converts MongoDoc to JSON
+      jsonArrayRoot.add(objectNode);
+      key = processed.lowerKey(key);
+    }
+    try {
+      return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(root);
+    } catch (JsonProcessingException jpe) {
+      //too lazy to add logger for this part.
+    }
+
+    return "{ \"sorry\" : \"Something failed internally. Please try again.\" }";
+
   }
 }
