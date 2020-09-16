f8e9252fa2c81f422c7b8568dd79e74aa155fcfd
Samuel O. Ugowe <sdougowe@gmail.com>
2020-09-16 14:06:51 -0400

    Making Mongo POJO changes current

--- src/main/java/org/galatea/starter/domain/MongoDocument.java
+++ src/main/java/org/galatea/starter/domain/MongoDocument.java
@@ -1,12 +1,12 @@
 package org.galatea.starter.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.Date;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Data public class MongoDocument {
+  private String date;
   @JsonProperty(value = "1. open") private Double open;
   @JsonProperty(value = "2. high") private Double high;
   @JsonProperty(value = "3. low") private Double low;
